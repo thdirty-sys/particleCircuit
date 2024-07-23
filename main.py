1920aa8bbb0fd5bbcc4b663f1da19ec1e13a004e
13thprad <tpd1302@gmail.com>
2024-07-23 02:07:29 +0100

    first?

--- main.py
+++ main.py
@@ -7,11 +7,14 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    wavy = 'caralho'
 
 # Press the green button in the gutter to run the script.
+wavy = 'nuts'
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+print(wavy)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ particleObj.py
@@ -0,0 +1,3 @@
+class Particle:
+    def __init__(self, start_pos):
+        self.pos = start_pos
