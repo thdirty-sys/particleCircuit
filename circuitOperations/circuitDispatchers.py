a25a338f2bd3efbc1771fe75e824468d86e62c81
13thprad <tpd1302@gmail.com>
2024-08-01 04:26:58 +0100

    Begun work on TASEP process across the circuit

--- circuitOperations/circuitDispatchers.py
+++ circuitOperations/circuitDispatchers.py
@@ -1,6 +1,7 @@
 from circuitOperations.particleObjs import *
 import numpy as np
 
+
 class CircuitDispatcher:
     """General Circuit dispatcher class."""
 
@@ -14,22 +15,22 @@ class CircuitDispatcher:
         ex_nodes = []
         repos = []
 
-        valid_repo_x = [i for i in range(2,48)]
+        valid_repo_x = [i for i in range(2, 48)]
 
         # Generate entry nodes w/ random positions and rates
-        for n in range(rng.integers(1,2)): #high was 6
-            new_node = Node((0, rng.integers(1,25)), rng.random())
+        for n in range(rng.integers(1, 2)):  # high was 6
+            new_node = Node((0, rng.integers(1, 25)), rng.random())
             en_nodes.append(new_node)
 
         # Generate exit nodes w/ random positions and rates
-        for n in range(rng.integers(1,2)): #high was 4
-            new_node = Node((49, rng.integers(1,25)), -rng.random())
+        for n in range(rng.integers(1, 2)):  # high was 4
+            new_node = Node((49, rng.integers(1, 25)), -rng.random())
             ex_nodes.append(new_node)
 
         # Generate randomly placed repositories w/ random capacities
-        for n in range(rng.integers(3,6)):
+        for n in range(rng.integers(3, 6)):
             chosen_x = rng.choice(valid_repo_x)
-            new_repo = Repository((chosen_x, rng.integers(1,25)), rng.integers(100, 1001))
+            new_repo = Repository((chosen_x, rng.integers(1, 25)), rng.integers(100, 1001))
             # For convenience later, repos is ordered by x pos
             if repos is []:
                 repos.append(new_repo)
@@ -43,10 +44,10 @@ class CircuitDispatcher:
 
             # To regulate distances between repositories
             valid_repo_x.remove(chosen_x)
-            if chosen_x+1 in valid_repo_x:
-                valid_repo_x.remove(chosen_x+1)
-            if chosen_x-1 in valid_repo_x:
-                valid_repo_x.remove(chosen_x-1)
+            if chosen_x + 1 in valid_repo_x:
+                valid_repo_x.remove(chosen_x + 1)
+            if chosen_x - 1 in valid_repo_x:
+                valid_repo_x.remove(chosen_x - 1)
 
         self.circuit = Circuit(repos, en_nodes, ex_nodes)
         return self.circuit
@@ -71,7 +72,20 @@ class TasepCircuitDispatcher(CircuitDispatcher):
 
             # Deals with case of entry node
             if chosen.name == "node":
-                c.particles.append(Particle(chosen.pos))
+                # Creates particle if empty with probability according to rate of node
+                if self.pos_empty(chosen.pos, c.particles):
+                    # With probability of node rate
+                    if rng.random() <= chosen.rate:
+                        c.particles.append(Particle(chosen.pos))
+
+            if chosen.name == "particle":
 
 
 
+
+    def pos_empty(self, pos, particle_list):
+        for p in particle_list:
+            if p.pos == pos:
+                return False
+        else:
+            return True
